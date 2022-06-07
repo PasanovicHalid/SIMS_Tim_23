4d7395a3edf26af0c68fa4936b0cba1e36ab9f56
Halid Pasanovic <pasanovic.ra114.2019@uns.ac.rs>
2022-06-07 14:03:09 +0200

    Ispravka sitna

--- ZdravoCorp/View/Manager/ViewModel/Rooms/CombineRoomsViewModel.cs
+++ ZdravoCorp/View/Manager/ViewModel/Rooms/CombineRoomsViewModel.cs
@@ -77,7 +77,7 @@ namespace ZdravoCorp.View.Manager.ViewModel.Rooms
             {
                 if (value != ContentViewModel.Instance.CurrentView)
                 {
-                    ContentViewModel.Instance.WindowBrosurwser.AddWindow(value);
+                    ContentViewModel.Instance.WindowBrowser.AddWindow(value);
                     ContentViewModel.Instance.CurrentView = value;
                     OnPropertyChanged();
                 }
