d48a45e381830d4d98d1d30038b66c724881e290
Bojana Karanovic <bojana.karanovic2015@gmail.com>
2022-06-11 15:22:09 +0200

    pocetak rada na vmmv obrascu

---
+++ ZdravoCorp/View/Patient/Modell/Allergies/Allergies.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ZdravoCorp.View.Core;
+
+namespace ZdravoCorp.View.Patient.Modell.Allergies
+{
+    public class Allergies : ObservableObject
+    {
+        private String other;
+        private String medicine;
+        private String note;
+
+        public Allergies()
+        {
+        }
+
+        public Allergies(string other, string medicine, string note)
+        {
+            this.other = other;
+            this.medicine = medicine;
+            this.note = note;
+        }
+
+        public String Other
+        {
+            get => other;
+            set
+            {
+                if (value != other)
+                {
+                    other = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public String Medicine
+        {
+            get => medicine;
+            set
+            {
+                if (value != medicine)
+                {
+                    medicine = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public String Note
+        {
+            get => note;
+            set
+            {
+                if (value != note)
+                {
+                    note = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+}
---
+++ ZdravoCorp/View/Patient/ViewModel/Allergies/AllergiesViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace ZdravoCorp.View.Patient.ViewModel.Allergies
+{
+    public class AllergiesViewModel
+    {
+        public ObservableCollection<String> MedicationAllergies { get; set; }
+        public ObservableCollection<String> OtherAllergies { get; set; }
+        public ObservableCollection<String> Note { get; set; }
+
+
+    }
+}
